f3ab57ca6fb717cb36e901e22814db26119dd4a0
ShubhamGupta1995 <guptapiyush.gupta12@gmail.com>
2020-08-07 16:28:51 +0530

    basic_software.sh added

---
+++ basic_software.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+## This script will install all the basic softwares in mac using brew 
+
+# Install Google chrome 
+brew cask install google-chrome
+
+# Install Firefox 
+brew cask install Firefox
+
+# Install Zoom
+brew cask install zoom
+
